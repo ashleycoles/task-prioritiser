6972d9556063e94851d8d1ebec8521a1d95f1f5a
Ash Coles <ashley@acoles.co.uk>
2025-01-16 14:14:40 +0000

    Working base task prioritisation

--- app/Services/TaskService.php
+++ app/Services/TaskService.php
@@ -3,17 +3,61 @@
 namespace App\Services;
 
 use App\Models\User;
+use Carbon\Carbon;
 
 class TaskService
 {
     public function getPrioritisedTasks(User $user)
     {
         // Default order: Priority
+        // Tasks overdue (deadline) by more than 5 days go to the top. If there are multiple, they are sorted by priority
+        // Tasks overdue (deadline) by less than 5 days go after, if multiple they are sorted by priority
+        // Followed by tasks due today, again in priority order if there are multiple
+        // Then any tasks not overdue are in priority order
         $tasks = $user->tasks()->orderBy('priority', 'desc')->get();
 
-        // Only return the highest priority tasks that don't exceed the hours the user works in a day
+        $today = Carbon::today();
+
+        $overdueByMoreThan5Days = [];
+        $overdueByLessThan5Days = [];
+        $dueToday = [];
+        $notOverdue = [];
+
+        foreach ($tasks as $task) {
+            $deadline = Carbon::parse($task->deadline);
+            $daysDifference = $today->diffInDays($deadline, false);
+
+            if ($daysDifference == 0) {
+                $dueToday[] = $task;
+            } elseif ($daysDifference < -5) {
+                $overdueByMoreThan5Days[] = $task;
+            } elseif ($daysDifference < 0) {
+                $overdueByLessThan5Days[] = $task;
+            } else {
+                $notOverdue[] = $task;
+            }
+        }
+
+        $this->prioritiseTasks($overdueByMoreThan5Days);
+        $this->prioritiseTasks($overdueByLessThan5Days);
+        $this->prioritiseTasks($dueToday);
+        $this->prioritiseTasks($notOverdue);
+
+        $prioritisedTasks = array_merge(
+            $overdueByMoreThan5Days,
+            $overdueByLessThan5Days,
+            $dueToday,
+            $notOverdue
+        );
+
+        // TODO: Only return the highest priority tasks that don't exceed the hours the user works in a day
         $availableDailyHours = $user->hours;
 
-        return $tasks;
+        return $prioritisedTasks;
+    }
+
+    private function prioritiseTasks(array &$tasks): void
+    {
+        usort($tasks, fn($a, $b) => $b->priority - $a->priority);
     }
 }
