613dab6ed42e320bdbf3235870e95205bf846f4d
Ash Coles <ashley@acoles.co.uk>
2025-01-17 14:25:57 +0000

    Aligning deadline colours with deadline groups

--- resources/js/Components/Deadline.vue
+++ resources/js/Components/Deadline.vue
@@ -1,30 +1,34 @@
 <script setup>
 
 import {computed} from "vue"
-import {differenceInDays, isBefore, parseISO, format} from "date-fns"
+import {differenceInDays, isBefore, parseISO, format, subDays} from "date-fns"
 
-const props = defineProps({
+const {deadline} = defineProps({
     deadline: String
 })
 
 const backgroundClass = computed(() => {
     const today = new Date()
-    const deadlineDate = new Date(props.deadline)
+    const deadlineDate = new Date(deadline)
     const daysDifference = differenceInDays(deadlineDate, today)
 
     if (daysDifference === 0) {
-        return "bg-orange-500"
+        return "bg-yellow-500"
+    }
+
+    if (isBefore(deadlineDate, subDays(today, 5))) {
+        return "bg-red-500"
     }
 
     if (isBefore(deadlineDate, today)) {
-        return "bg-red-500"
+        return "bg-orange-500"
     }
 
     return  "bg-green-500"
 })
 
 const formattedDeadline = computed(() => {
-    const deadlineDate = parseISO(props.deadline)
+    const deadlineDate = parseISO(deadline)
     return format(deadlineDate, "dd/MM/yyyy")
 })
 
