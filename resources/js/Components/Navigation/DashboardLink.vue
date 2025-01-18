65a182df1a1f5270cca77a61f2bb6b37c2f2d8c4
Ash Coles <ashley@acoles.co.uk>
2025-01-18 15:01:19 +0000

    Basic dashboard

---
+++ resources/js/Components/Navigation/DashboardLink.vue
@@ -0,0 +1,16 @@
+<script setup>
+import {Link} from "@inertiajs/vue3";
+
+defineProps({
+    href: {
+        type: String,
+        required: true
+    }
+})
+</script>
+
+<template>
+    <Link class="border-2 rounded-sm py-8 text-center text-xl hover:bg-gray-200" :href="href" >
+        <slot />
+    </Link>
+</template>
--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,7 +1,8 @@
 <script setup>
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
-import { Head } from '@inertiajs/vue3';
+import { Head, Link } from '@inertiajs/vue3';
 import H1 from "@/Components/Text/H1.vue";
+import DashboardLink from "@/Components/Navigation/DashboardLink.vue";
 </script>
 
 <template>
@@ -13,11 +14,15 @@ import H1 from "@/Components/Text/H1.vue";
         </template>
 
         <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
-            <div
-                class="overflow-hidden bg-white shadow-sm sm:rounded-lg"
-            >
-                <div class="p-6 text-gray-900">
-                    You're logged in!
+            <div class="overflow-hidden bg-white shadow-sm sm:rounded-lg">
+                <div class="grid grid-cols-4 gap-2 p-6 text-gray-900">
+                    <DashboardLink :href="route('tasks.index')">
+                        View Tasks
+                    </DashboardLink>
+
+                    <DashboardLink :href="route('tasks.create')">
+                        Add Task
+                    </DashboardLink>
                 </div>
             </div>
         </div>
